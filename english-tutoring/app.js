7a961555f2a1323f74999ac4d60af145a450f25f
Rebeca Parreiras <166830435+rebecaparreiras@users.noreply.github.com>
2025-02-04 11:14:19 -0300

    projeto inicial

---
+++ english-tutoring/app.js
@@ -0,0 +1,9 @@
+// Menu mobile
+let menuIcon = document.querySelector('#menu-icon')
+let menu = document.querySelector('.menu')
+
+menuIcon.onclick = () => {
+    menu.classList.toggle('active');
+};
+
+
